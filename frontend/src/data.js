eb32de6ab85dd8283210fff1dc92e0c9db01ce1a
Loubna Sellam <loubnasellam49@gmail.com>
2023-09-29 20:25:45 +0200

    git commit

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Ensemble de sport femme',
+            slug: 'Ensemble de sport femme',
+            category: 'T-shirt',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'Ensemble de haute qualité',
+
+        },
+
+        {
+            name: 'Un gilet imprimé',
+            slug: 'Un gilet imprimé',
+            category: 'T-shirt',
+            image: '/images/p2.jpg',
+            price: 50,
+            countInStock: 0,
+            brand: 'Mongo',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'Gilet de haute qualité',
+
+        },
+
+        {
+            name: 'Blazer femme',
+            slug: 'Blazer femme',
+            category: 'Pantalons',
+            image: '/images/p3.jpg',
+            price: 90,
+            countInStock: 150,
+            brand: 'Zara',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'Blazer de haute qualité',
+        },
+        {
+            name: 'Robe Rose Poudré',
+            slug: 'Robe Rose Poudré',
+            category: 'Pantalons',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 500,
+            brand: 'Massimo dutti',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Robe de haute qualité',
+        }    
+    ],
+};
+
+export default data; 
