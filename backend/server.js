a905d39355f599c7e29ce140497910b4a5274896
Loubna Sellam <loubnasellam49@gmail.com>
2023-09-30 10:30:28 +0200

    git commit

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,19 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
+});
+
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Aucun Produits Trouvé' });
+  }
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port,  () => {
-    console.log(`Serveur sur l'adresse suivante http:/localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Serveur sur l'adresse suivante http:/localhost:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Link, Route,  Routes } from 'react-router-dom'; 
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -8,30 +8,31 @@ import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
-    <div className="d-flex flex-column site-container">
-      <header>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <LinkContainer to="/">
-            <Navbar.Brand>Dz-Shopop</Navbar.Brand>
-            </LinkContainer>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Dz-Shopop</Navbar.Brand>
+              </LinkContainer>
             </Container>
-        </Navbar>
-
-    </header>
-    <main>
-      <Container>
-      <Routes>
-        <Route path="/product/:slug" element={<ProductScreen />} />
-        <Route path="/" element={<HomeScreen />} />
-      </Routes>
-      </Container>
-      </main>
-      <footer>
-        <div className="text-center">All right reserved by LOUBNA SELLAM </div>
-      </footer>
-  </div>
-  </BrowserRouter>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-4">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">
+            All right reserved by LOUBNA SELLAM{' '}
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
